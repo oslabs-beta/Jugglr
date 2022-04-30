6c2527de8b9cc94659d904b10d8cca13200dacea
Alvin Ma <alvin.ma95@gmail.com>
2022-04-30 10:14:24 -0400

    mantine Styling template

--- src/client/components/Create.tsx
+++ src/client/components/Create.tsx
@@ -1,5 +1,6 @@
-import {Button, TextInput, Modal,Text, Center} from '@mantine/core'
+import {Button, TextInput, Modal,Tooltip, Text, Center} from '@mantine/core'
 import { useState } from 'react';
+import {AlertCircle} from 'tabler-icons-react'
 
 const Create = () => {
     
@@ -11,12 +12,19 @@ const Create = () => {
           console.log("Electron's Response:", response);
           setPath(response);
           console.log("New Path:", path);
+          if(response!==""){
+          setOpened(true);
+          }
         };
        const [opened,setOpened] = useState(false)
+      const rightSection = <Tooltip label="Provide file path to root directory" placement="start" withArrow><AlertCircle size={16}></AlertCircle></Tooltip>
+      const empty = path === "" ? true : false
+      
     return (
       <>
-        <TextInput
       
+      <span><TextInput
+       style={{width:"80%"}}
       label="Schema File Name"
       description="Upload Schema File"
       variant="filled"
@@ -24,12 +32,28 @@ const Create = () => {
       required
       value={path}
      
-      error="Please upload a valid file"
-    /><Button  onClick={handleClick}>Upload file </Button>
-    <Modal opened={opened} onClose={()=> setOpened(false)} title="Please upload a valid file">
+      error={empty}
+    />
+    
+    
+    <Button  style={{marginTop: "2%"}} radius="md" onClick={handleClick}>Upload file </Button></span>
+    <Modal opened={opened} onClose={()=> setOpened(false)} title="Success!">
       {/* <Text><Center>Please upload a valid file</Center></Text> */}
     </Modal>
-    <Button onClick={()=>setOpened(true)}> click me </Button>
+    <br></br>
+
+    
+    <TextInput
+      style={{width:"80%", marginTop: "2%"}}
+      label="Project Root Directory"
+      variant="filled"
+      disabled
+      required
+      rightSection={rightSection}
+     
+    />
+    
+    <Button disabled={empty} style={{marginTop: "2%"}} radius="md" onClick={()=>setOpened(true)}> click me </Button>
       </>
     );
   };
--- src/client/components/Run.tsx
+++ src/client/components/Run.tsx
@@ -1,11 +1,11 @@
-import {Select, Input , SelectChevronIcon} from "@mantine/core"
+import {Select, Input , SelectChevronIcon, Textarea} from "@mantine/core"
 
 const Run = () => {
     
     return (
       <div>
-      <Select rightSectionWidth={2}  placeholder="select existing image" label="Image" data={['Image1','Image2']} />
-      
+      <Select  style={{width:"80%"}}  placeholder="select existing image" label="Image" data={['Image1','Image2']} />
+      <Textarea style={{width:"80%", marginTop:"5%" }}placeholder="Docker Command Line" label="Docker Command Line" autosize/>
       {/* <Input component="select" rightSection={<SelectChevronIcon size="md" error="Sorry!" color="blue"/>}>
         <option value="1">1</option>
         <option value="2">2</option>
