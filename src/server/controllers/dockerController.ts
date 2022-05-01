6e2b51df9a2fd6a8e18b83d6893c74992a8d05bd
S M Iftekhar Uddin <Uddin.SMI@gmail.com>
2022-05-01 19:42:01 -0400

    commit before pulling/merging dev

--- src/server/controllers/dockerController.ts
+++ src/server/controllers/dockerController.ts
@@ -11,17 +11,17 @@ const Docker = require('dockerode');
 ////frontend:: 
 ////frontend:: user selecting a project directory (rootDir) will invoke file.mkJugglr function. 
 //For e.g., invoke file.mkJugglr() when passing path data to rootDir variable?
-const rootDir = path.join(__dirname);
+const rootDir: string = path.join(__dirname);
 const dbName = 'db name';
 const dbUser = 'db username';
 const dbPwd = 'db password';
 const imageName = 'image_name';
-const schemaFilePath = 'schema-filepath'; //users will select their sql schema file
+const schemaFilePath = 'schema-filepath'; //users will select their own sql schema file
 
 const containerName = 'container-name';
 const containerPort = '7676';
 
-
+const pathDockerfile =  path.join(rootDir, 'Jugglr/Dockerfile');
 const dockerfileContents = 
 `FROM postgres:latest /
 ENV POSTGRES_USER ${dbUser} /
@@ -30,17 +30,15 @@ ENV POSTGRES_DB ${dbName} /
 WORKDIR ${rootDir}
 COPY ${schemaFilePath} /docker-entrypoint-initdb.d `
 
-const pathDockerfile =  path.join(rootDir, 'Dockerfile');
-
 
 
 
 const file = {
-  //creating Jugglr folder inside rootDir. Also checks if Jugglr already exists in rootDir
+  //Creates Jugglr folder inside rootDir. Also checks if Jugglr already exists in rootDir
   mkJugglr: function () {
     fs.mkdir(path.join(rootDir, 'Jugglr'), (err) => {
       if (err) {
-        return console.log('Jugglr folder already exists', err); //need to test this out
+        return console.log('Jugglr folder already exists', err); 
       }
       console.log('Jugglr folder created successfully!');
     });
@@ -50,18 +48,25 @@ const file = {
 
 file.mkJugglr();
 
+
+
+
+
 const dockerController = {
-  ////frontend:: user clicks 'Generate Docker File' button. onClick(?) calls the dockerController.createDockerfile function
+  ////frontend:: user clicks 'Generate Docker File' button. 
+    //onClick(?) calls the dockerController.createDockerfile function, passing in pathDockerfile and dockerfileContents
   createDockerfile: function (filePath, fileContent) {
     fs.writeFile(filePath, fileContent, (err) => {
       if (err) { console.log(err);}
       else { console.log('Dockerfile created successfully')}
       //will overwrite any existing Dockerfile in the folder
     });
-  },
+  }, 
+
 
   ////frontend:: user clicks 'Create Docker Image' button. onClick(?) invokes dockerController.createImage
   createImage: function () {
+    //image isn't being created atm
     const docker1 = new Docker({socketPath: '/var/run/docker.sock'});
     const pathway = path.join(rootDir, 'Jugglr');
     docker1.buildImage({
@@ -75,34 +80,30 @@ const dockerController = {
   },
 
 
-
     
   //// frontend:: user enters container name and port, and clicks 'Run New Container' button
-  //onClick: transfer input field info to variables containerName and containerPort 
-  //AND then trigger dockerController.createContainer function 
+    //onClick: transfer input field info to variables containerName and containerPort 
+    //AND then trigger dockerController.runNewContainer function 
 
   runNewContainer: function () {
-    // issue: obtain containerid from funtion's local score to global scope
+    // issue: obtain containerid from funtion's local scope to global scope
     // image and port hardcoded atm
     const docker1 = new Docker({socketPath: '/var/run/docker.sock'});
-    const containerId = {};
+    // const containerId = {};
     docker1.createContainer({
       Image: 'postgresdb', 
       Cmd: ['/bin/bash'], 
       name: containerName, 
-      HostConfig: {PortBindings: {'7474/tcp': [{ HostPort: '7474' }]}}}, 
-    function (err, container) {
-      containerId.id = container.id;
+      HostConfig: {PortBindings: {'7474': [{ HostPort: '7474' }]}}}, 
+    function (_err, container) {
+      // containerId.id = container.id;
       container.start(function (err, data) {
-        //   console.log('inside container.start');
-        //   console.log('err', err, 'data', data);
+          console.log('err', err, 'data', data);
       });
-      console.log('containerId', containerId);
-      //send container name and id to a database
-      return containerId;
+      //send container name and id to a database?
+      // return containerId;
     }
     );    
-    console.log('outside', containerId);
   },
 
   //frontend:: user clicks stop/delete/start container buttons. 
@@ -113,7 +114,7 @@ const dockerController = {
     const selectedContainer = docker1.getContainer(`${containerName}`);
         
     selectedContainer.start(function (err, data) {
-      // console.log(data);
+      console.log('err', err, 'data', data);
       console.log('inside start container');
     });
   },
@@ -121,7 +122,7 @@ const dockerController = {
   //have to pass in containerId to function
   stopContainer: function () {
     const docker1 = new Docker({socketPath: '/var/run/docker.sock'});
-    docker1.listContainers(function (err, containers) {
+    docker1.listContainers(function (_err, containers) {
       containers.forEach(function (containerInfo) {
         console.log('inside containers.forEach');
         docker1.getContainer(containerInfo.Id).stop();
@@ -135,6 +136,7 @@ const dockerController = {
     const docker1 = new Docker({socketPath: '/var/run/docker.sock'});
     docker1.listContainers(function (err, containers) {
       // console.log('List all containers', containers);
+      console.log('err', err);
       const allContainers = [ containers ];
     });
   },
@@ -146,17 +148,15 @@ const dockerController = {
 
 }
 
+
+
+
 dockerController.createDockerfile(pathDockerfile, dockerfileContents);
+dockerController.createImage();
+dockerController.runNewContainer();
+dockerController.getContainers();
 
 
-module.exports = fileController;
+
 module.exports = dockerController;
 
-
-
-/*
-PGDATABASE= "$PGDATABASE" || 'postgres'
-PGUSER= "$PGUSER" || 'postgres'
-PGPASSWORD= "$PGPASSWORD" || 'postgres'
-PGPORT= "$DBPORT" || 5432
-*/
