ee08ea16136c7b95b5ea13310ce27826b107994d
StanislausA <anthonystanislaus1@gmail.com>
2022-05-04 19:50:40 -0400

    pre merge

---
+++ __tests__/ProjectConfig.test.ts
@@ -0,0 +1,8 @@
+import React from "React";
+import { Provider } from "react-redux";
+import UserEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("Unit tests for ProjectConfig component", () => {
+  
+});
