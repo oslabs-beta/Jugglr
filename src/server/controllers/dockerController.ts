f8cbf3bc448a19f37d095f6ca8567522f0db3952
Alvin Ma <99209691+alvma1945@users.noreply.github.com>
2022-05-14 11:37:04 -0400

    Update dockerController.ts

--- src/server/controllers/dockerController.ts
+++ src/server/controllers/dockerController.ts
@@ -259,11 +259,11 @@ const dockerController = {
       const stream = await docker.run(image, ['postgres'], process.stdout, {
         Env: [`POSTGRES_PASSWORD=${process.env.POSTGRES_PASSWORD}`], WorkingDir: process.env.ROOTDIR, name: containerName, HostConfig: { PortBindings: {
           "5432/tcp" : [ { "HostPort": `${port}` } ] }}, Tty: false})
+       stream.once('error', function err (_err) {
+          event.sender.send('runResult', false);
+        })
         stream.once('container', async function container (_container) {
           event.sender.send('runResult', true);
-      })
-      stream.once('error', function err (_err) {
-        event.sender.send('runResult', false);
       })
      
       return true;
