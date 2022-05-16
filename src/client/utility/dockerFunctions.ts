0db0214374f577687755fc425a237fbc38c7a2fb
Alvin Ma <alvin.ma95@gmail.com>
2022-05-16 17:31:07 -0400

    update font and add tooltip to loadData

--- src/client/utility/dockerFunctions.ts
+++ src/client/utility/dockerFunctions.ts
@@ -1,35 +1,16 @@
-import { container, DockerFile, EnvConfig, LoadTable, StartUpObj ,image} from "../../types";
+import { cleanNotifications } from "@mantine/notifications";
+import { container, StartUpObj ,image} from "../../types";
 
 
-/**
- * Action helpers moved here for time being...
- */
+//StartupConfig functions
 
-
-/**
- * Call electron to create a DockerFile with given details
- * @param {DockerFile} values
- * @returns {boolean}
- */
-export const setDockerFile = async (values: DockerFile): Promise<boolean> => {
-  return await dockController.createDockerfile(values);
+export const getImages = async () : Promise<image[]> => {
+  return await dockController.getImagesList()
 }
 
-/**
- * call electron to set rootDir env variable
- * @param {EnvConfig} values 
- * @returns {object}
- */
-
-/**
- * Extracts just the image name from array of image objects obtained from docker
- * @param arr - arr param is an array of image objects
- * @returns returns an array of image names that are shown as the dropdown values
- */
 export const destructureImageList = (arr:image[]): string[] => {
   const newImageList :string[] = ['']
 
-  
   arr.forEach((ele)=>{
     const curTag: string[] = ele['repoTags']
     if(curTag!==null){
@@ -41,12 +22,43 @@ export const destructureImageList = (arr:image[]): string[] => {
   return newImageList
 }
 
+export const buildOrRun = async (values:StartUpObj, action:string, callback:Function) => {
+  if(action==='buildImage'){
+    buildImage(values.image)
+    await dockController.buildImageResult((args:boolean|string|Error)=>{
+     callback(args)
+    })
+    
+  } else {
+     runNewContainer(values)
+    await dockController.runNewResult((args:boolean|string)=>{
+    callback(args)
+    })
+  }
+}
+
+const buildImage = async (image:string):Promise<void> => {
+  console.log('buildImage')
+return await dockController.buildImage(image);
+}
+
+const runNewContainer = async (values:StartUpObj): Promise<string> => {
+  const imageValue = values.selectedImage
+  const containerName = values.container
+  const port = values.port+'' 
+  const response = await dockController.runContainer(imageValue,containerName,port)
+  return response
+}
+
+  //RunConfig Functions
 /**
  * 
- * @param arr 
- * @returns 
+ * @param containerId 
+ * @param action 
+ * @param callback 
  */
-export const destructureContainerList = (arr:container[]):string[] => {
+
+ export const destructureContainerList = (arr:container[]):string[] => {
   
   const newContainerList:string[] = ['']
   
@@ -59,7 +71,6 @@ export const destructureContainerList = (arr:container[]):string[] => {
   
   return newContainerList
 }
-
 export const destructureContainerId = (arr:container[]) => {
   
   const containerIdObj = {}
@@ -74,77 +85,69 @@ export const destructureContainerId = (arr:container[]) => {
   
   return containerIdObj
 }
-
-export const runNewContainer = async (values:StartUpObj): Promise<string> => {
-  const imageValue = values.selectedImage
-  const containerName = values.container
-  const port = values.port+'' 
-  const response = await dockController.runContainer(imageValue,containerName,port)
-  return response
-   
-}
-export const buildImage = async (image:string):Promise<void> => {
-    console.log('buildImage')
-  return await dockController.buildImage(image);
-}
- 
-export const getImages = async () : Promise<image[]> => {
-  return await dockController.getImagesList()
-}
-
-export const startContainer = async(containerId:string): Promise<boolean| string > => {
- 
-if(containerId===undefined){
-    return false;
-}
-const response = await dockController.startContainer(containerId)
-return response
-}
-
-export const stopContainer = async(containerId:string):Promise<boolean> => {
-if(containerId===undefined){
-    return false;
-}
-const response = await dockController.stopContainer(containerId)
-return response
- 
-}
-
-export const removeContainer = async(containerId: string): Promise<boolean> => {
-  if(containerId ===undefined){
-    return false;
-  }
-  const response = await dockController.removeContainer(containerId)
-  return response
-}
-
-export const destructureContainerPort = (container:container[]):string[] => {
-const newArray = ['']
-  container.forEach((ele)=>{
-    newArray.push(""+ele.port)
-  })
-return newArray
-}
-
-export const modifyErrorRemove = (errorMessage: string)=>{
-  const arr=errorMessage.split(' ')
-  const newArr = arr.slice(0,6)
-  return newArr.join(' ')
-}
-
-export const buildOrRun = async (values:StartUpObj, action:string, callback:Function) => {
-    if(action==='buildImage'){
-      buildImage(values.image)
-      await dockController.buildImageResult((args:boolean|string|Error)=>{
-        //   console.log('dockFunctions',args, typeof args)
-       callback(args)
+export  const startStopOrRemove = async (containerId: string, action:'start' | 'stop'| 'remove', callback:Function) :Promise<void> => {
+    //clear all notifications so only one notification is shown at any given time
+    cleanNotifications(); 
+    console.log(action)
+    if(action==='start'){
+      await startContainer(containerId)
+    //After invoking start container, listen for response from backend.  Will receive either true or false as a response
+      await dockController.startContainerResult((arg:boolean | string)=>{
+        //function below will determine what message is displayed in app 
+        callback(arg)
       })
-      
+    } else if (action ==='stop') {
+      await stopContainer(containerId)
+      await dockController.stopContainerResult((arg:boolean | string)=>{
+        callback(arg)
+      })
     } else {
-       runNewContainer(values)
-      await dockController.runNewResult((args:boolean|string)=>{
-      callback(args)
+      await removeContainer(containerId)
+      await dockController.removeContainerResult((arg: boolean|string)=>{
+      callback(arg)
       })
+      }
+  
     }
-  }
+  
+    const startContainer = async(containerId:string): Promise<boolean| string > => {
+ 
+      if(containerId===undefined){
+          return false;
+      }
+      const response = await dockController.startContainer(containerId)
+      return response
+      }
+      
+      const stopContainer = async(containerId:string):Promise<boolean> => {
+      if(containerId===undefined){
+          return false;
+      }
+      const response = await dockController.stopContainer(containerId)
+      return response
+       
+      }
+      
+       const removeContainer = async(containerId: string): Promise<boolean> => {
+        if(containerId ===undefined){
+          return false;
+        }
+        const response = await dockController.removeContainer(containerId)
+        return response
+      }
 
+      export const modifyErrorRemove = (errorMessage: string)=>{
+        const arr=errorMessage.split(' ')
+        const newArr = arr.slice(0,6)
+        return newArr.join(' ')
+      }
+
+//LoadDataConfig functions
+
+      export const destructureContainerPort = (container:container[]):string[] => {
+        const newArray = ['']
+          container.forEach((ele)=>{
+            newArray.push(""+ele.port)
+          })
+        return newArray
+        }
