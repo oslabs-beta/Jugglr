451886c9b5850558c39dbf839c0ab97e8d48971a
StanislausA <anthonystanislaus1@gmail.com>
2022-05-16 11:04:37 -0400

    reverted changes

--- src/client/reducers/envConfigSlice.ts
+++ src/client/reducers/envConfigSlice.ts
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const envConfigSlice = createSlice({
   name: "envConfig",
   initialState: {
-    rootDir: "/Users/anthony/Desktop/Just WOW/VScode_Worktable/Codesmith/OSP/Jugglr",
+    rootDir: "",
     image: "",
     container: "",
     from: "postgres:latest",
@@ -13,7 +13,7 @@ export const envConfigSlice = createSlice({
     database: "",
     password: "",
     port: "5432",
-    schema: "/Users/anthony/Desktop/Just WOW/VScode_Worktable/Codesmith/OSP/Jugglr/__tests__/data/species.csv",
+    schema: "",
     tablePath: "",
     tableName:"",
     dropDownImage:[""],
